f13beb8207a13a7b115d97a81d25a6133b429aa7
Samantha Gatt <smfgatt@gmail.com>
2020-05-31 11:22:12 -0400

    Adds zombie node

--- ZombieConga/GameScene.swift
+++ ZombieConga/GameScene.swift
@@ -11,6 +11,8 @@ import GameplayKit
 
 class GameScene: SKScene {
     
+    var zombie = SKSpriteNode(imageNamed: "zombie1")
+    
     override func didMove(to view: SKView) {
         backgroundColor = SKColor.black
         let background = SKSpriteNode(imageNamed: "background1")
@@ -25,5 +27,9 @@ class GameScene: SKScene {
         // Makes sure background is always drawn first
         background.zPosition = -1
         addChild(background)
+        
+        zombie.position = CGPoint(x: 400, y: 400)
+        zombie.setScale(2)
+        addChild(zombie)
     }
 }
